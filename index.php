ae7f89e0f361edfd818dcee4cbb72f322235584e
ivanel <hp.ivan@gmail.com>
2022-03-21 23:08:08 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html> 
+	<head> 
+	<title>PHP Test</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>PHP Test</h1>
+		<p><b>An Example of PHP in Action</b></p>
+		<p><b>This is an example line added by ivanel</b></p>
+		<p><b>This is an example second line added by ivanel</b></p>
+		<?php echo "The Current Date and Time is: <br />"; 
+		echo date("g:i A l, F j Y.");?> </p>
+	<h2>PHP Information</h2> 
+		<p> <?php phpinfo(); ?> </p> 
+        <h2>GOEDEMIDDAG GEZELLIGE GRIEZELS UIT GRONINGEN</h2>
+	</body> 
+</html>
